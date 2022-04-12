7ed6a3e8730e18bc4b243aeeb0149d2123f8bd19
stevtang <stevenmtang1@gmail.com>
2022-04-11 17:22:36 -0700

    Finished stack

--- stack.js
+++ stack.js
@@ -39,19 +39,35 @@ class Stack {
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
+    if(this.size === 0) throw new Error("Error: stack is empty");
 
+    let returnVal = this.top.val;
+
+    if(this.size === 1){
+      this.top = null;
+      this.size = 0;
+      return returnVal;
+    }
+
+    this.top = this.top.next;
+    this.size--;
+
+    return returnVal;
   }
 
   /** peek(): return the value of the top node in the stack. */
 
   peek() {
-
+    if(this.size === 0) throw new Error("Error: stack is empty");
+    const removeVal = this.top.val;
+    return removeVal;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    if (this.top === null) return true;
+    return false;
   }
 }
 
