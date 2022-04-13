cc5596d2cb93422712512ce0bafb2560ed3bf132
hectorchu~ <hector.chu828@gmail.com>
2022-04-12 17:05:07 -0700

    Finished bracket balancer

--- further-study/further-study.js
+++ further-study/further-study.js
@@ -5,6 +5,33 @@ function stringReversal(str) {
 }
 
 function balancedBrackets(str) {
+
+    let stack = new Stack();
+
+    for(let char of str) {
+        if(char === "{" || char === "[" || char === "(") {
+            stack.push(char);
+        } else if(char === "}") {
+            if(stack.isEmpty()) return false;
+            if(stack.peek() === "{") stack.pop();
+            else {
+                return false;
+            }
+        } else if(char === "]") {
+            if(stack.isEmpty()) return false;
+            if(stack.peek() === "[") stack.pop();
+            else {
+                return false;
+            }
+        } else if(char === ")") {
+            if(stack.isEmpty()) return false;
+            if(stack.peek() === "(") stack.pop();
+            else {
+                return false;
+            }
+        }     
+    }
+    return (stack.isEmpty()) ? true : false;
 }
 
 function josephusSurvivor(peopleNum, skip) {
